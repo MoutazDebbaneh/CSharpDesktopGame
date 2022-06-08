6e6b8c4a82020514f0ed786fbbbcc9972a83662e
Moutaz Debbaneh <matazdev@gmail.com>
2022-06-08 17:34:54 +0300

    Added an empty viewport with the necessary files

--- CSharpDesktopGame/CSharpDesktopGame/Program.cs
+++ CSharpDesktopGame/CSharpDesktopGame/Program.cs
@@ -1,15 +1,64 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+using System.Drawing;
+using OpenTK.Input;
 
 namespace CSharpDesktopGame
 {
-    class Program
+    class Program : GameWindow
     {
+        public static string TITLE = "C# Desktop Game - Mini Project";
+
+        public static int WIDTH = 600;
+        public static int HEIGHT = 760;
+
+        public static int textureId;
+
+        public Program() : base(WIDTH, HEIGHT, GraphicsMode.Default, TITLE) { }
+
+
+        protected override void OnLoad(EventArgs e)
+        {
+
+            base.OnLoad(e);
+            GL.ClearColor(Color.FromArgb(0, 191, 255));
+
+            GL.Enable(EnableCap.Texture2D);
+            GL.Enable(EnableCap.Blend);
+            GL.BlendFunc(BlendingFactorSrc.SrcAlpha, BlendingFactorDest.OneMinusSrcAlpha);
+            
+        }
+
+
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+
+        }
+
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            base.OnUpdateFrame(e);
+
+        }
+
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            base.OnRenderFrame(e);
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+
+            SwapBuffers();
+        }
+
         static void Main(string[] args)
         {
+            Program program = new Program();
+            program.Run();
         }
+
     }
 }
