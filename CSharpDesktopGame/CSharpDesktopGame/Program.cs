541c6d4a9d51da645f725e6d7f5277b68c74ed0c
Moutaz Debbaneh <matazdev@gmail.com>
2022-06-13 15:11:23 +0300

    Screen Size & BG & Moving basket

--- CSharpDesktopGame/CSharpDesktopGame/Program.cs
+++ CSharpDesktopGame/CSharpDesktopGame/Program.cs
@@ -14,9 +14,23 @@ namespace CSharpDesktopGame
         public static int WIDTH = 600;
         public static int HEIGHT = 760;
 
-        public static int textureId;
+        public static float BASKET_SPEED = 0.05f;
+        public static float BASKET_HALF_WIDTH = 0.2f;
 
-        public Program() : base(WIDTH, HEIGHT, GraphicsMode.Default, TITLE) { }
+        public static float BASKET_UPPER_BOUND = -0.17f;
+        public static float BASKET_LOWER_BOUND = -0.97f;
+
+        public static float BASKET_TOP_POS = -0.75f;
+        public static float BASKET_BOTTOM_POS = -0.95f;
+
+        public static int BackgroundTextureId, BasketTextureId;
+
+        private float basketDx = 0, basketDy = 0;
+
+        public Program() : base(WIDTH, HEIGHT, GraphicsMode.Default, TITLE)
+        {
+            this.initWindow();
+        }
 
 
         protected override void OnLoad(EventArgs e)
@@ -28,7 +42,9 @@ namespace CSharpDesktopGame
             GL.Enable(EnableCap.Texture2D);
             GL.Enable(EnableCap.Blend);
             GL.BlendFunc(BlendingFactorSrc.SrcAlpha, BlendingFactorDest.OneMinusSrcAlpha);
-            
+            BackgroundTextureId = Utilities.LoadTexture(@"Images\bg.png");
+            BasketTextureId = Utilities.LoadTexture(@"Images\basket.png");
+
         }
 
 
@@ -43,6 +59,34 @@ namespace CSharpDesktopGame
         {
             base.OnUpdateFrame(e);
 
+            if (Mouse[MouseButton.Left])
+            {
+                Console.WriteLine(this.Location.ToString());
+            }
+
+            if (Keyboard[Key.Right])
+            {
+                if(basketDx + BASKET_SPEED + BASKET_HALF_WIDTH < 1)
+                    basketDx += BASKET_SPEED;
+            }
+
+            if (Keyboard[Key.Left])
+            {
+                if (basketDx - BASKET_SPEED - BASKET_HALF_WIDTH > -1)
+                    basketDx -= BASKET_SPEED;
+            }
+
+            if (Keyboard[Key.Up])
+            {
+                if (basketDy + BASKET_SPEED + BASKET_TOP_POS < BASKET_UPPER_BOUND)
+                    basketDy+= BASKET_SPEED;
+            }
+
+            if (Keyboard[Key.Down])
+            {
+                if (basketDy - BASKET_SPEED + BASKET_BOTTOM_POS > BASKET_LOWER_BOUND)
+                    basketDy -= BASKET_SPEED;
+            }
         }
 
 
@@ -51,13 +95,88 @@ namespace CSharpDesktopGame
             base.OnRenderFrame(e);
             GL.Clear(ClearBufferMask.ColorBufferBit);
 
+
+            renderBackground();
+            renderBasket();
+
+
+
             SwapBuffers();
+        }
+
+        private void renderBackground()
+        {
+
+            GL.BindTexture(TextureTarget.Texture2D, BackgroundTextureId);
+
+            GL.Begin(BeginMode.Polygon);
+
+            GL.TexCoord2(1, 0);
+            GL.Vertex2(1, 1);
+
+            GL.TexCoord2(1, 1);
+            GL.Vertex2(1, -1);
+
+            GL.TexCoord2(0, 1);
+            GL.Vertex2(-1, -1);
+
+            GL.TexCoord2(0, 0);
+            GL.Vertex2(-1, 1);
+
+            GL.BindTexture(TextureTarget.Texture2D, 0);
+            GL.End();
+        }
+
+
+        private void renderBasket()
+        {
+            GL.BindTexture(TextureTarget.Texture2D, BasketTextureId);
+
+            GL.Begin(BeginMode.Polygon);
+
+            GL.TexCoord2(1, 0);
+            GL.Vertex2(BASKET_HALF_WIDTH + basketDx, BASKET_TOP_POS + basketDy);
+
+            GL.TexCoord2(1, 1);
+            GL.Vertex2(BASKET_HALF_WIDTH + basketDx, BASKET_BOTTOM_POS + basketDy);
+
+            GL.TexCoord2(0, 1);
+            GL.Vertex2(-BASKET_HALF_WIDTH + basketDx, BASKET_BOTTOM_POS + basketDy);
+
+            GL.TexCoord2(0, 0);
+            GL.Vertex2(-BASKET_HALF_WIDTH + basketDx, BASKET_TOP_POS + basketDy);
+
+            GL.BindTexture(TextureTarget.Texture2D, 0);
+            GL.End();
+        }
+
+        private void initWindow()
+        {
+            int width = DisplayDevice.Default.Width;
+            int height = DisplayDevice.Default.Height;
+            Point startingPoint;
+            if (width == 1920 && height == 1080)
+            {
+                startingPoint = new Point(761 - (WIDTH / 2), 10);
+            }
+            else if(width == 1366 && height == 768)
+            {
+                startingPoint = new Point(676 - (WIDTH / 2), 10);
+                this.Height = 680;
+            }
+            else
+            {
+                return;
+            }
+            this.Location = startingPoint;
         }
 
         static void Main(string[] args)
         {
+            
             Program program = new Program();
             program.Run();
+            
         }
 
     }
